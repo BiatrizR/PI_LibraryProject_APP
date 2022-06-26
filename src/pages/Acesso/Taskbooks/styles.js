3c98f4df6c1bc5910dd7f3407a11489962024d8c
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2022-06-26 17:12:50 -0300

    Update styles.js
    definindo style

--- src/pages/Acesso/Taskbooks/styles.js
+++ src/pages/Acesso/Taskbooks/styles.js
@@ -1,1 +1,16 @@
+import styled from 'styled-components/native';
 
+
+export const View = styled.View`
+background-color: #fff;
+flex:1;
+align-items: center;
+
+`;
+
+export const ScreenStack = styled.View`
+background-color: #fff;
+flex:1;
+align-items: center;
+
+`;
